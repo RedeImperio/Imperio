3766bbc75ffdfebeec9da95c71986ebbcab215e8
lucasof <lucazof@proton.me>
2023-05-14 20:23:35 -0300

    Criado Plugin ImperioStaff

---
+++ ImperioStaff/src/main/java/top/redeimperio/imperiostaff/ImperioStaff.java
@@ -0,0 +1,27 @@
+package top.redeimperio.imperiostaff;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import top.redeimperio.imperiostaff.commands.StaffChatCommand;
+
+public final class ImperioStaff extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getConsoleSender().sendMessage("§9ImperioStaff §7ativado com §asucesso!");
+
+        EnableCommands();
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage("§9ImperioStaff §7foi §cdesligado!");
+    }
+
+    public void EnableCommands(){
+        getCommand("sc").setExecutor(new StaffChatCommand());
+    }
+}
---
+++ ImperioStaff/src/main/java/top/redeimperio/imperiostaff/commands/StaffChatCommand.java
@@ -0,0 +1,47 @@
+package top.redeimperio.imperiostaff.commands;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StaffChatCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Verificar se o comando foi executado por um jogador
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Este comando só pode ser executado por jogadores.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        // Verificar a permissão do jogador
+        if (!player.hasPermission("imperio.staff")) {
+            player.sendMessage("Você não tem permissão para executar este comando.");
+            return true;
+        }
+
+        // Verificar se a mensagem foi fornecida
+        if (args.length == 0) {
+            player.sendMessage("Uso incorreto do comando. Utilize /sc <mensagem>");
+            return true;
+        }
+
+        // Construir a mensagem a ser enviada aos staffs
+        StringBuilder messageBuilder = new StringBuilder();
+        for (String arg : args) {
+            messageBuilder.append(arg).append(" ");
+        }
+        String message = messageBuilder.toString().trim();
+
+        // Enviar mensagem aos staffs online
+        for (Player staff : player.getServer().getOnlinePlayers()) {
+            if (staff.hasPermission("imperio.staff")) {
+                staff.sendMessage("[StaffChat] " + player.getName() + ": " + message);
+            }
+        }
+
+        return true;
+    }
+}
