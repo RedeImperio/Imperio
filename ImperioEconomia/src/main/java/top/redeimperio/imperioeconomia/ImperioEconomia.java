7086e229d6d10a5fdf5d69694de6838b7430910c
lukazof <lucascraft101@gmail.com>
2023-06-23 15:23:29 -0300

    Adicionado verificação de Magnata no ImperioEconomia

--- ImperioEconomia/src/main/java/top/redeimperio/imperioeconomia/ImperioEconomia.java
+++ ImperioEconomia/src/main/java/top/redeimperio/imperioeconomia/ImperioEconomia.java
@@ -1,15 +1,16 @@
 package top.redeimperio.imperioeconomia;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -18,9 +19,16 @@ public class ImperioEconomia extends JavaPlugin {
     private Map<UUID, Double> playerBalances;
     private FileConfiguration playerDataConfig;
     private File playerDataFile;
+    private UUID magnataId;
+    private double magnataBalance;
 
     @Override
     public void onEnable() {
+        FileConfiguration config = getConfig();
+        config.options().copyDefaults(true);
+        saveConfig();
+        int magnataCheckInterval = getConfig().getInt("magnataCheckInterval", 300);
+
         // Inicializa o mapa de saldos dos jogadores
         playerBalances = new HashMap<>();
 
@@ -31,6 +39,9 @@ public class ImperioEconomia extends JavaPlugin {
         }
         playerDataConfig = YamlConfiguration.loadConfiguration(playerDataFile);
         loadPlayerBalances();
+
+        // Agendando a tarefa de verificação a cada x segundos
+        Bukkit.getScheduler().runTaskTimer(this, this::checkMagnata, 0, magnataCheckInterval * 20);
     }
 
     @Override
@@ -48,15 +59,48 @@ public class ImperioEconomia extends JavaPlugin {
             }
 
             Player player = (Player) sender;
-            double balance = getPlayerBalance(player.getUniqueId());
-            player.sendMessage("Seu saldo é: " + balance + " coins.");
-            return true;
+
+            if (args.length >= 2 && args[0].equalsIgnoreCase("set")) {
+                String playerName = args[1];
+                double amount;
+
+                try {
+                    amount = Double.parseDouble(args[2]);
+                } catch (NumberFormatException e) {
+                    player.sendMessage("§cValor inválido. Use um número válido para o saldo.");
+                    return true;
+                }
+
+                setPlayerBalance(playerName, amount);
+                player.sendMessage("§aO saldo de §6" + playerName + "§a foi definido como §f" + amount + "§a coins.");
+                return true;
+            } else if (args.length >= 1 && args[0].equalsIgnoreCase("top")) {
+                sendTopRichestPlayers(player);
+                return true;
+            } else {
+                double balance = getPlayerBalance(player.getUniqueId());
+                player.sendMessage("§aSeu saldo é: §f" + balance + "§a coins.");
+                return true;
+            }
         }
         return false;
     }
 
     private double getPlayerBalance(UUID playerId) {
         return playerBalances.getOrDefault(playerId, 0.0);
+    }
+
+    private void setPlayerBalance(String playerName, double amount) {
+        Player targetPlayer = getServer().getPlayer(playerName);
+
+        if (targetPlayer != null) {
+            UUID playerId = targetPlayer.getUniqueId();
+            playerBalances.put(playerId, amount);
+            playerDataConfig.set(playerId.toString(), amount);
+            savePlayerBalances();
+        } else {
+            getLogger().warning("§cJogador não encontrado: §f" + playerName);
+        }
     }
 
     private void loadPlayerBalances() {
@@ -76,6 +120,53 @@ public class ImperioEconomia extends JavaPlugin {
             playerDataConfig.save(playerDataFile);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void sendTopRichestPlayers(Player player) {
+        List<Map.Entry<UUID, Double>> sortedBalances = new ArrayList<>(playerBalances.entrySet());
+        sortedBalances.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        player.sendMessage("§aTOP 10 MAIS RICOS:");
+        int count = 1;
+        for (Map.Entry<UUID, Double> entry : sortedBalances) {
+            UUID playerId = entry.getKey();
+            double balance = entry.getValue();
+            String playerName = getPlayerName(playerId);
+            player.sendMessage("§f" + count + ". §6" + playerName + "§f - " + balance + " coins");
+            count++;
+            if (count > 10) {
+                break;
+            }
+        }
+    }
+
+    private String getPlayerName(UUID playerId) {
+        Player player = getServer().getPlayer(playerId);
+        if (player != null) {
+            return player.getName();
+        }
+        return "Jogador Offline";
+    }
+
+    private void checkMagnata() {
+        double maxBalance = 0;
+        UUID newMagnataId = null;
+
+        for (Map.Entry<UUID, Double> entry : playerBalances.entrySet()) {
+            UUID playerId = entry.getKey();
+            double balance = entry.getValue();
+            if (balance > maxBalance) {
+                maxBalance = balance;
+                newMagnataId = playerId;
+            }
+        }
+
+        if (newMagnataId != null && (magnataId == null || !newMagnataId.equals(magnataId))) {
+            magnataId = newMagnataId;
+            magnataBalance = maxBalance;
+            String playerName = getPlayerName(magnataId);
+            Bukkit.broadcastMessage("\n§f " + playerName + "§a é o novo §6MAGNATA§a do servidor com a quantia de §f" + magnataBalance + "§a coins!\n");
         }
     }
 }
