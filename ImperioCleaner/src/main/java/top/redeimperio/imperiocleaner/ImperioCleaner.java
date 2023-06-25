d18113df17f4397be1dda7b8a496512130c21f36
lukazof <lucascraft101@gmail.com>
2023-06-24 22:13:39 -0300

    Adicionado ImperioCleaner

---
+++ ImperioCleaner/src/main/java/top/redeimperio/imperiocleaner/ImperioCleaner.java
@@ -0,0 +1,74 @@
+package top.redeimperio.imperiocleaner;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class ImperioCleaner extends JavaPlugin {
+
+    private int taskId;
+
+    @Override
+    public void onEnable() {
+        startCleanupTask();
+    }
+
+    @Override
+    public void onDisable() {
+        cancelCleanupTask();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("limpar")) {
+            removeItemsFromGround();
+            Bukkit.broadcastMessage("§c( ! ) Os itens foram removidos do chão!");
+            return true;
+        }
+        return false;
+    }
+
+    private void startCleanupTask() {
+        BukkitRunnable task = new BukkitRunnable() {
+            int countdown = 300;
+
+            @Override
+            public void run() {
+                if (countdown == 300) {
+                    Bukkit.broadcastMessage("§c(!) Os itens serão removidos do chão em 5 minutos!");
+                } else if (countdown == 60) {
+                    Bukkit.broadcastMessage("§c(!) Os itens serão removidos do chão em 1 minuto!");
+                } else if (countdown <= 3) {
+                    Bukkit.broadcastMessage("§c(!) Os itens serão removidos do chão em " + countdown + " segundos!");
+                }
+
+                if (countdown <= 0) {
+                    removeItemsFromGround();
+                    Bukkit.broadcastMessage("§c(!) Os itens foram removidos do chão!");
+                    cancelCleanupTask();
+                } else {
+                    countdown--;
+                }
+            }
+        };
+
+        taskId = task.runTaskTimer(this, 20L, 20L).getTaskId();
+    }
+
+    private void cancelCleanupTask() {
+        Bukkit.getScheduler().cancelTask(taskId);
+    }
+
+    private void removeItemsFromGround() {
+        for (World world : Bukkit.getWorlds()) {
+            for (Item item : world.getEntitiesByClass(Item.class)) {
+                item.remove();
+            }
+        }
+    }
+}
